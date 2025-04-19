0a63600cd15594f48d47203203667cd44581e273
ScaredRabbit <scaredrabbitnl@gmail.com>
2025-04-19 11:02:54 +0200

    neoforge

--- src/main/java/io/github/scaredsmods/rabbilib/RabbiLib.java
+++ src/main/java/io/github/scaredsmods/rabbilib/RabbiLib.java
@@ -1,15 +1,22 @@
 package io.github.scaredsmods.rabbilib;
 
 import com.mojang.logging.LogUtils;
-import net.fabricmc.api.ModInitializer;
+
+import net.neoforged.bus.api.IEventBus;
+import net.neoforged.fml.ModContainer;
+import net.neoforged.fml.common.Mod;
 import org.slf4j.Logger;
 
-public class RabbiLib implements ModInitializer {
+@Mod(RabbiLib.MOD_ID)
+public class RabbiLib  {
 
 
     public static final String MOD_ID = "rabbilib";
     public static final Logger LOGGER = LogUtils.getLogger();
-    @Override
-    public void onInitialize() {
+
+    public RabbiLib (IEventBus bus, ModContainer container) {
+
+
     }
+
 }
